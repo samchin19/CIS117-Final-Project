232504dcf0466e6f874b4f0fee88810e0170a07e
Sam <chins1@my.smccd.edu>
2025-07-20 09:22:53 -0700

    customer.py

--- customer.py
+++ customer.py
@@ -1,1 +1,17 @@
+# CIS117 - Final Part 1 - Maksym Stesev, Micole Chen, Samantha Chin
+# Team: SF Pythons
 
+TAX_RATE = 0.09
+
+class Customer:
+    def__init__(self):
+        pass
+    def calculate_tax(self, subtotal):
+        return subtotal * TAX_RATE
+
+class Student(Customer):
+    def calculate_tax(self, subtotal):
+        return 0.0
+
+class RegularCustomer(Customer):
+    pass
