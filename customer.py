dcfecaf617f815c4908b4a807cf0087dc4334a62
Sam <chins1@my.smccd.edu>
2025-07-20 09:25:26 -0700

    customer.py

--- customer.py
+++ customer.py
@@ -1,6 +1,10 @@
 # CIS117 - Final Part 1 - Maksym Stesev, Micole Chen, Samantha Chin
 # Team: SF Pythons
 
+'''
+create customer as a superclass with an inheritable calculate tax method.
+student tax is set to 0.
+'''
 TAX_RATE = 0.09
 
 class Customer:
