6358c1c5397a95c01fa73cb804146d3aeb0e1bb8
theMaximmus <m.stesev@gmail.com>
2025-07-21 18:36:39 -0700

    Added main.py file and started working on the GUI.

---
+++ main.py
@@ -0,0 +1,26 @@
+# CIS117 - Final Part 1 - Maksym Stesev, Micole Chen, Samantha Chin
+# Team: SF Pythons
+
+import tkinter
+
+
+def displayOrderGUI(self):
+    print("Displaying the order in a Graphics Window")
+
+    gui_frame = tkinter.Frame(self.root)
+    gui_frame.pack()
+
+    # Adding "Order Summary" label
+    order_label = tkinter.Label(gui_frame, text="Your Order:")
+    order_label.grid(row=0, column=0, columnspan=2, pady=10)
+
+    # Adding label for each Order Item
+
+    # Adding "Total" label
+    total_label = tkinter.Label(gui_frame, text=f"Total: ${self.price:.2f}") # TODO: Change "price" to an actual variable
+
+    # Adding "Close" Button
+    close_button = tkinter.Button(gui_frame, text="Close application", command=self.root.destroy)
+    close_button.grid(row=len(self._orderDict) +2, column=0, columnspan=2, pady=10) # TODO: Change "_orderDict" to an actual variable
+
+    self.root.mainloop()
