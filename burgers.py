4e11ba17359f8e0a24895f3f54353f4c6dcbf4db
Sam <chins1@my.smccd.edu>
2025-07-28 13:52:00 -0700

    Update burgers.py

--- burgers.py
+++ burgers.py
@@ -16,19 +16,19 @@ class DeAnzaBurger(Burger):
         super().__init__("De Anza Burger", 5.25)
 
 class BaconCheese(Burger):
-    def __init__(self)
-    super().__init__("Bacon Cheese", 5.75)
+    def __init__(self):
+        super().__init__("Bacon Cheese", 5.75)
 
 class MushroomSwiss(Burger):
-    def __init__(self)
-    super().__init__("Mushroom Swiss", 5.95)
+    def __init__(self):
+        super().__init__("Mushroom Swiss", 5.95)
 
 class WesternBurger(Burger):
-    def __init__(self)
-    super().__init__("Western Burger", 5.95)
+    def __init__(self):
+        super().__init__("Western Burger", 5.95)
 
 class DonCaliBurger(Burger):
-    def __init__(self)
-    super().__init__("Don Cali Burger", 5.95)
+    def __init__(self):
+        super().__init__("Don Cali Burger", 5.95)
     
 BURGER_CLASSES = [DeAnzaBurger, BaconCheese, MushroomSwiss, WesternBurger, DonCaliBurger]
