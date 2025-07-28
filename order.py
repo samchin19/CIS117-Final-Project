8cefbe7ad68af05e38cddeccc65ee79d1453d6b8
Sam <chins1@my.smccd.edu>
2025-07-28 13:54:13 -0700

    Update order.py

--- order.py
+++ order.py
@@ -4,9 +4,6 @@
 from burgers import BURGER_CLASSES
 from customer import Student, RegularCustomer
 
-from burgers import BURGER_CLASSES
-from customer import Student, RegularCustomer
-
 class Order:
     def __init__(self):
         self.items = []
@@ -17,7 +14,7 @@ class Order:
     def calc_total(self, customer):
         subtotal = sum(burger.get_price() * quantity for burger, quantity in self.items)
         tax = customer.calculate_tax(subtotal)
-        total = subtotal + tax
+        total = subtotal +tax
         return subtotal, tax, total
 
     def print_bill(self, subtotal, tax, total):
@@ -28,19 +25,20 @@ class Order:
             print(f"{burger.name} x {quantity} - ${burger.get_price() * quantity:.2f}")
 
         print("*" * 50)
-        print(f"Total before tax: ${subtotal}:.2f}")
+        print(f"Total before tax: ${subtotal:.2f}")
         print(f"Tax Amount: ${tax:.2f}")
-        print(f"Total price afteer tax: ${total:.2f}\n")
+        print(f"Total price after tax: ${total:.2f}\n")
 
     def save_bill_to_file(self, subtotal, tax, total):
-        with open("bill.txt", "w") as file: 
+        with open("bill.txt.", "w") as file:
             file.write("Your Bill:\n")
             file.write("*" * 50 + "\n")
             
             for burger, quantity in self.items:
-                file.write(f"{burger.name} x {quantity} - ${burger.get.price() * quantity:.2f}\n")
+                file.write(f"{burger.name} x {quantity} - ${burger.get_price() * quantity:.2f}\n")
             file.write("*" * 50 + "\n")
             
             file.write(f"Total before tax: ${subtotal:.2f}\n")
             file.write(f"Tax Amount: ${tax:.2f}\n")
             file.write(f"Total price after tax: ${total:.2f}\n")
+            
