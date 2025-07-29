f3ed481b229accaea930fb5f1be7c6a0aa7a7d67
theMaximmus <m.stesev@gmail.com>
2025-07-28 19:17:36 -0700

    Rewritten Customer.py

--- Customer.py
+++ Customer.py
@@ -10,9 +10,24 @@ student tax is set to 0.
 TAX_RATE = 0.09
 
 class Customer:
+    '''
+    This class represents any user that interacts with the program.
+    It serves as a superclass, providing a base method for calculating tax
+    on a subtotal.
+    '''
 
     def __init__(self):
+        '''
+        Initializes a new Customer instance.
+
+
+        '''
         pass
 
     def calculate_tax(self, subtotal):
+        '''
+        Calculates the tax amount for a given subtotal.
+        :param subtotal:
+        :return:
+        '''
         return subtotal * TAX_RATE
