aacbbc3d1a1d9f4688b8184b8fafa07e02cfa6a7
Sam <chins1@my.smccd.edu>
2025-07-29 15:02:50 -0700

    Update Order.py

--- Order.py
+++ Order.py
@@ -1,6 +1,8 @@
 # CIS117 - Final Part 1 - Maksym Stesev, Micole Chen, Samantha Chin
 # Team: SF Pythons
 
+from Burgers import Burger
+
 class Order:
     def __init__(self):
         self.items = []
